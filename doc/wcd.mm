51f19e4a99ba847dd801ac235e24b0c67f0fc0b1
sergey-selivanov <sergey_selivanov@hotmail.com>
2016-04-16 20:30:14 +0300

    build, todo list

---
+++ doc/wcd.mm
@@ -0,0 +1,13 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1460826806843" ID="ID_1092541838" MODIFIED="1460826823711" TEXT="Web Cache Digger">
+<node CREATED="1460826830783" ID="ID_704671263" MODIFIED="1460826833965" POSITION="right" TEXT="TODO">
+<node CREATED="1460826869348" ID="ID_275198656" MODIFIED="1460826880926" TEXT="update all to recent versions"/>
+<node CREATED="1460826883742" ID="ID_258931481" MODIFIED="1460826895119" TEXT="build webstart"/>
+<node CREATED="1460826934146" ID="ID_919189953" MODIFIED="1460826951203" TEXT="migrate existing database to latest h2 format"/>
+<node CREATED="1460826895820" ID="ID_1174399941" MODIFIED="1460826933384" TEXT="flywaydb"/>
+<node CREATED="1460826963880" ID="ID_269747893" MODIFIED="1460826968570" TEXT="build zip"/>
+<node CREATED="1460826969551" ID="ID_1482405823" MODIFIED="1460826981155" TEXT="build installers"/>
+</node>
+</node>
+</map>
